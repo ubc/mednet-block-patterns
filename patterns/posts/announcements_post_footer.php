cfc3bdde61ff0a5e092ade183848489f8d28b57b
Graham Douglas <graham.douglas@ubc.ca>
2022-08-04 18:15:30 -0700

    fixed post patterns

---
+++ patterns/posts/announcements_post_footer.php
@@ -0,0 +1,74 @@
+<!-- wp:group {"className":"announcements-post-footer"} -->
+<div class="wp-block-group announcements-post-footer"><!-- wp:paragraph {"style":{"typography":{"fontSize":"14px","textTransform":"uppercase","letterSpacing":"0.7px","fontStyle":"normal","fontWeight":"600"}}} -->
+<p style="font-size:14px;font-style:normal;font-weight:600;text-transform:uppercase;letter-spacing:0.7px">EXPLORE MORE OF WHAT&rsquo;S NEW</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:query {"queryId":33,"query":{"perPage":"3","pages":"0","offset":"0","postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false,"taxQuery":{"category":[5]}},"displayLayout":{"type":"flex","columns":3},"className":"announcement_feed_small"} -->
+<div class="wp-block-query announcement_feed_small"><!-- wp:post-template -->
+<!-- wp:post-featured-image {"isLink":true,"align":"wide"} /-->
+
+<!-- wp:post-title {"isLink":true,"linkTarget":"_blank","style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","letterSpacing":"-0.18px"},"color":{"text":"#002145"}}} /-->
+
+<!-- wp:post-excerpt {"style":{"typography":{"fontSize":"16px"},"color":{"text":"#002145"}}} /-->
+<!-- /wp:post-template -->
+
+<!-- wp:query-pagination {"align":"center","className":"mednet_pagination_block"} -->
+<!-- wp:query-pagination-previous /-->
+
+<!-- wp:query-pagination-numbers /-->
+
+<!-- wp:query-pagination-next /-->
+<!-- /wp:query-pagination -->
+
+<!-- wp:query-no-results -->
+<!-- wp:paragraph {"placeholder":"Add text or blocks that will display when the query returns no results."} -->
+<p>No results found.</p>
+<!-- /wp:paragraph -->
+<!-- /wp:query-no-results --></div>
+<!-- /wp:query -->
+
+<!-- wp:group -->
+<div class="wp-block-group"><!-- wp:columns {"textColor":"white","className":"blue_feature_blocks"} -->
+<div class="wp-block-columns blue_feature_blocks has-white-color has-text-color"><!-- wp:column {"width":"33.33%","style":{"color":{"background":"#002145"}},"textColor":"white"} -->
+<div class="wp-block-column has-white-color has-text-color has-background" style="background-color:#002145;flex-basis:33.33%"><!-- wp:heading {"level":3,"textColor":"white"} -->
+<h3 class="has-white-color has-text-color">Calendar</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color">Lorem ipsum dolor sit amet, adipisicing elit, sed do eiusmod tempor incididunt ut ero labore.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color"><a rel="noreferrer noopener" href="https://mednet.med.ubc.ca/" data-type="URL" data-id="https://mednet.med.ubc.ca/" target="_blank">Calendar of Events</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color">Faculty of Medicine News</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"66.66%","style":{"color":{"background":"#0055b7"}},"textColor":"white"} -->
+<div class="wp-block-column has-white-color has-text-color has-background" style="background-color:#0055b7;flex-basis:66.66%"><!-- wp:group -->
+<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","letterSpacing":"-0.24px"}},"textColor":"white"} -->
+<h3 class="has-white-color has-text-color" style="font-size:24px;letter-spacing:-0.24px">Stories</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"textColor":"white"} -->
+<p class="has-white-color has-text-color">Learn more about What&rsquo;s New in the Faculty of Medicine and at UBC.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"white","style":{"typography":{"fontSize":"16px"},"border":{"radius":"0px"},"color":{"background":"#002145"}},"className":"is-style-fill"} -->
+<div class="wp-block-button has-custom-font-size is-style-fill" style="font-size:16px"><a class="wp-block-button__link has-white-color has-text-color has-background" href="/" style="border-radius:0px;background-color:#002145" target="_blank" rel="noreferrer noopener">See all News &amp; Honours <i class="icon fa fa-clone" aria-hidden="true"> </i></a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
