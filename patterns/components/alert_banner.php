5d8cf8ae7242e0568c322848b2deb0a161afe7c7
Graham Douglas <graham.douglas@ubc.ca>
2022-08-09 13:27:10 -0700

    Bugfix/homepage (#6)
    * Homepage edits, quicklinks.
    
    * events list component added to home
    
    * refactoring the staff spotlight page
    
    * Fixes
    
    * MAS-5817: One column page styling and accordion fixed
    
    * MAS-5754: line height is adjusted
    
    * Homepage edits, quicklinks.
    
    * events list component added to home
    
    * implemented full-bleed on home
    
    * added spotlight as component home
    
    * content-card fixes
    
    * homepage spotlight edits
    
    * removed empty css rules
    
    Co-authored-by: Darya Shokouhi <darya.shokouhi@ubc.ca>

--- patterns/components/alert_banner.php
+++ patterns/components/alert_banner.php
@@ -1,5 +1,41 @@
 <!-- wp:group {"className":"alert-banner"} -->
-<div class="wp-block-group alert-banner"><!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","letterSpacing":"0px"},"color":{"text":"#0055b7"}}} -->
-<p class="has-text-color" style="color:#0055b7;font-size:16px;letter-spacing:0px"><span class="closebtn" onclick="closeAlert()">&times;</span><i class="icon-exclamation-sign icon-white"></i><strong>Alert Banner</strong>: For your December 31 pay, the deadline to enter and submit your time in Workday is December 21. Managers must approve by December 22.</p>
-<!-- /wp:paragraph --></div>
+<div class="wp-block-group alert-banner">
+    <!-- wp:columns {"isStackedOnMobile":false,"className":"no-margin"} -->
+    <div class="wp-block-columns is-not-stacked-on-mobile no-margin">
+        <!-- wp:column {"verticalAlignment":"center","width":"30px"} -->
+        <div
+            class="wp-block-column is-vertically-aligned-center"
+            style="flex-basis: 30px"
+        >
+            <!-- wp:paragraph -->
+            <p><i class="icon-exclamation-sign icon-white alert-icon"></i></p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"width":"100%"} -->
+        <div class="wp-block-column" style="flex-basis: 100%">
+            <!-- wp:paragraph {"style":{"typography":{"fontSize":"16px","letterSpacing":"0px"}}} -->
+            <p style="font-size: 16px; letter-spacing: 0px">
+                <strong>Alert Banner</strong>: For your December 31 pay, the
+                deadline to enter and submit your time in Workday is December
+                21. Managers must approve by December 22.
+            </p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+
+        <!-- wp:column {"verticalAlignment":"top","width":"25px"} -->
+        <div
+            class="wp-block-column is-vertically-aligned-top"
+            style="flex-basis: 25px"
+        >
+            <!-- wp:paragraph -->
+            <p><span class="closebtn" onclick="closeAlert()">&times;</span></p>
+            <!-- /wp:paragraph -->
+        </div>
+        <!-- /wp:column -->
+    </div>
+    <!-- /wp:columns -->
+</div>
 <!-- /wp:group -->
---
+++ patterns/components/event_list_item.php
@@ -0,0 +1,15 @@
+<!-- wp:group {"className":"event-list-item"} -->
+<div class="wp-block-group event-list-item">
+    <!-- wp:heading {"level":3,"className":"event_title"} -->
+    <h3 class="event_title">
+        <a href="/" target="_blank" rel="noreferrer noopener"
+            >Title</a
+        >
+    </h3>
+    <!-- /wp:heading -->
+
+    <!-- wp:paragraph {"className":"event_date"} -->
+    <p class="event_date">May 2, 2022</p>
+    <!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
