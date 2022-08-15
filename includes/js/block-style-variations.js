93b050a1add752adc72bad397d761015b9c29173
Graham Douglas <graham.douglas@ubc.ca>
2022-08-15 16:45:48 -0700

    Removed core button styles, added light + dark

--- includes/js/block-style-variations.js
+++ includes/js/block-style-variations.js
@@ -1,32 +1,45 @@
-wp.blocks.registerBlockStyle('core/group', {
-    name: 'hide-group',
-    label: 'Hide',
-});
-wp.blocks.registerBlockStyle('core/read-more', {
-    name: 'hide-read-more',
-    label: 'Hide',
-});
-wp.blocks.registerBlockStyle('core/group', {
-    name: '3-4-width',
-    label: '3/4 Width',
-});
-wp.blocks.registerBlockStyle('core/group', {
-    name: '1-4-width',
-    label: '1/4 Width',
-});
-wp.blocks.registerBlockStyle('core/group', {
-    name: '1-2-width',
-    label: '1/2 Width',
-});
+wp.domReady(() => {
+    wp.blocks.unregisterBlockStyle(
+        'core/button',
+        ['outline', 'fill']
+    );
+    wp.blocks.registerBlockStyle('core/group', {
+        name: 'hide-group',
+        label: 'Hide',
+    });
+    wp.blocks.registerBlockStyle('core/read-more', {
+        name: 'hide-read-more',
+        label: 'Hide',
+    });
+    wp.blocks.registerBlockStyle('core/group', {
+        name: '3-4-width',
+        label: '3/4 Width',
+    });
+    wp.blocks.registerBlockStyle('core/group', {
+        name: '1-4-width',
+        label: '1/4 Width',
+    });
+    wp.blocks.registerBlockStyle('core/group', {
+        name: '1-2-width',
+        label: '1/2 Width',
+    });
 
-wp.blocks.registerBlockStyle(
-    'core/quote', {
-    name: 'blue-quote',
-    label: 'MedNet Quote',
-});
+    wp.blocks.registerBlockStyle(
+        'core/quote', {
+        name: 'blue-quote',
+        label: 'MedNet Quote',
+    });
 
-wp.blocks.registerBlockStyle(
-    'core/button', {
-    name: 'blue-btn',
-    label: 'Blue',
+    wp.blocks.registerBlockStyle(
+        'core/button', [
+        {
+            name: 'blue-btn',
+            label: 'Default',
+            isDefault: true,
+        },
+        {
+            name: 'light-blue',
+            label: 'Dark Blue'
+        }
+    ]);
 });
