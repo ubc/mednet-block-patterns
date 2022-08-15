9516c471e3a6fc44854f4deca1c44fcb7c2efae0
Graham Douglas <graham.douglas@ubc.ca>
2022-08-15 14:02:54 -0700

    updated image sizes and mobile site padding

--- patterns/pages/faculty_staff_spotlight_page.php
+++ patterns/pages/faculty_staff_spotlight_page.php
@@ -1,51 +1,97 @@
 <!-- wp:group {"className":"services_resources_page"} -->
-<div class="wp-block-group services_resources_page"><!-- wp:group {"className":"overview_pages"} -->
-    <div class="wp-block-group overview_pages"><!-- wp:group {"className":"spotlight_title_with_big_bio_photo_page"} -->
-    <div class="wp-block-group spotlight_title_with_big_bio_photo_page"><!-- wp:group {"className":"services_resources_page"} -->
-    <div class="wp-block-group services_resources_page"><!-- wp:group {"className":"overview_pages"} -->
-    <div class="wp-block-group overview_pages"><!-- wp:group {"className":"spotlight_title_with_big_bio_photo_page"} -->
-    <div class="wp-block-group spotlight_title_with_big_bio_photo_page"><!-- wp:heading {"textAlign":"center","level":1,"fontSize":"large"} -->
-    <h1 class="has-text-align-center has-large-font-size">Staff Spotlight</h1>
-    <!-- /wp:heading --></div>
-    <!-- /wp:group --></div>
-    <!-- /wp:group --></div>
+<div class="wp-block-group services_resources_page">
+    <!-- wp:group {"className":"overview_pages"} -->
+    <div class="wp-block-group overview_pages">
+        <!-- wp:group {"className":"spotlight_title_with_big_bio_photo_page"} -->
+        <div class="wp-block-group spotlight_title_with_big_bio_photo_page">
+            <!-- wp:group {"className":"services_resources_page"} -->
+            <div class="wp-block-group services_resources_page">
+                <!-- wp:group {"className":"overview_pages"} -->
+                <div class="wp-block-group overview_pages">
+                    <!-- wp:group {"className":"spotlight_title_with_big_bio_photo_page"} -->
+                    <div
+                        class="wp-block-group spotlight_title_with_big_bio_photo_page"
+                    >
+                        <!-- wp:heading {"textAlign":"center","level":1,"fontSize":"large"} -->
+                        <h1 class="has-text-align-center has-large-font-size">
+                            Staff Spotlight
+                        </h1>
+                        <!-- /wp:heading -->
+                    </div>
+                    <!-- /wp:group -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+
+            <!-- wp:group {"className":"faculty_staff_spotlight_page"} -->
+            <div class="wp-block-group faculty_staff_spotlight_page">
+                <!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+                <p class="has-text-align-center has-medium-font-size">
+                    <strong
+                        >Meet staff members from across the Faculty of
+                        Medicine</strong
+                    >. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua.
+                </p>
+                <!-- /wp:paragraph -->
+
+                <!-- wp:query {"queryId":18,"query":{"perPage":50,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false}} -->
+                <div class="wp-block-query">
+                    <!-- wp:post-template {"className":"spotlight-single-post"} -->
+                    <!-- wp:post-title /-->
+
+                    <!-- wp:post-featured-image {"sizeSlug":"medium_large"} /-->
+
+                    <!-- wp:post-excerpt /-->
+                    <!-- /wp:post-template -->
+                </div>
+                <!-- /wp:query -->
+
+                <!-- wp:group {"style":{"color":{"background":"#0055b7"}},"className":"spotlight-submission-footer"} -->
+                <div
+                    class="wp-block-group spotlight-submission-footer has-white-color has-text-color has-background"
+                    style="background-color: #0055b7"
+                >
+                    <!-- wp:heading -->
+                    <h2>Spotlight Submissions</h2>
+                    <!-- /wp:heading -->
+
+                    <!-- wp:paragraph -->
+                    <p>
+                        If you are interested in being profiled as part of the
+                        spotlight series or would like to nominate a colleague,
+                        email
+                        <a
+                            href="mailto:communications.med@ubc.ca"
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            >communications.med@ubc.ca</a
+                        >.
+                    </p>
+                    <!-- /wp:paragraph -->
+
+                    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+                    <div class="wp-block-buttons">
+                        <!-- wp:button {"style":{"color":{"background":"#002145"}},"className":"is-style-blue-btn"} -->
+                        <div class="wp-block-button is-style-blue-btn">
+                            <a
+                                class="wp-block-button__link has-background"
+                                style="background-color: #002145"
+                                >Contact Us</a
+                            >
+                        </div>
+                        <!-- /wp:button -->
+                    </div>
+                    <!-- /wp:buttons -->
+                </div>
+                <!-- /wp:group -->
+            </div>
+            <!-- /wp:group -->
+        </div>
+        <!-- /wp:group -->
+    </div>
     <!-- /wp:group -->
-    
-    <!-- wp:group {"className":"faculty_staff_spotlight_page"} -->
-    <div class="wp-block-group faculty_staff_spotlight_page"><!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
-    <p class="has-text-align-center has-medium-font-size">
-        <strong>Meet staff members from across the Faculty of Medicine</strong>.
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua.
-      </p>
-    <!-- /wp:paragraph -->
-    
-    <!-- wp:query {"queryId":18,"query":{"perPage":50,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false}} -->
-    <div class="wp-block-query"><!-- wp:post-template {"className":"spotlight-single-post"} -->
-    <!-- wp:post-title /-->
-    
-    <!-- wp:post-featured-image /-->
-    
-    <!-- wp:post-excerpt /-->
-    <!-- /wp:post-template --></div>
-    <!-- /wp:query -->
-    
-    <!-- wp:group {"style":{"color":{"background":"#0055b7"}},"className":"spotlight-submission-footer"} -->
-    <div class="wp-block-group spotlight-submission-footer has-white-color has-text-color has-background" style="background-color:#0055b7"><!-- wp:heading -->
-    <h2>Spotlight Submissions</h2>
-    <!-- /wp:heading -->
-    
-    <!-- wp:paragraph -->
-    <p>If you are interested in being profiled as part of the spotlight series or would like to nominate a colleague, email <a href="mailto:communications.med@ubc.ca" target="_blank" rel="noreferrer noopener">communications.med@ubc.ca</a>.</p>
-    <!-- /wp:paragraph -->
-    
-    <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
-    <div class="wp-block-buttons"><!-- wp:button {"style":{"color":{"background":"#002145"}},"className":"is-style-blue-btn"} -->
-    <div class="wp-block-button is-style-blue-btn"><a class="wp-block-button__link has-background" style="background-color:#002145">Contact Us</a></div>
-    <!-- /wp:button --></div>
-    <!-- /wp:buttons --></div>
-    <!-- /wp:group --></div>
-    <!-- /wp:group --></div>
-    <!-- /wp:group --></div>
-    <!-- /wp:group --></div>
-    <!-- /wp:group -->
+</div>
+<!-- /wp:group -->
