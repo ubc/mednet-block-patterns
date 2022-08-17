cbff5e6f3e4927eb34c67eb329518db865318f56
Graham Douglas <graham.douglas@ubc.ca>
2022-08-17 12:39:25 -0700

    Added eslint, wp standards formatted js

--- includes/js/block-variations.js
+++ includes/js/block-variations.js
@@ -1,21 +1,46 @@
-wp.blocks.registerBlockVariation(
-	'core/group',
-	{
-		name: 'full-width-group',
-		title: 'Full width group',
-		attributes: {
-			align: 'full'
-		}
+const { select, subscribe } = wp.data;
+
+const closeListener = subscribe(() => {
+	const isReady = select('core/editor').__unstableIsEditorReady();
+	if (!isReady) {
+		// Editor not ready.
+		return;
 	}
-);
-wp.blocks.registerBlockVariation(
-	'core/group',
-	{
-		name: 'full-width-group',
-		title: 'Full width group',
+	// Close the listener as soon as we know we are ready to avoid an infinite loop.
+	closeListener();
+	// Your code is placed after this comment, once the editor is ready.
+	registerQueryVariation();
+});
+
+function getCurrentPostId() {
+	const post_id = wp.data.select('core/editor').getCurrentPostId();
+	console.log('post is ', wp.data.select('core/editor').getCurrentPostId());
+	return post_id;
+}
+
+function registerQueryVariation() {
+	wp.blocks.registerBlockVariation('core/query', {
+		name: 'query-content',
+		title: 'new query',
 		attributes: {
-			align: 'full'
+			query: {
+				exclude: getCurrentPostId(),
+			},
 		},
-		isDefault: true
-	}
-);
+		innerBlocks: [
+			['core/post-template', [['core/post-title'], ['core/post-date']]],
+			['core/query-pagination'],
+			['core/query-no-results'],
+		],
+		isDefault: true,
+	});
+}
+
+wp.blocks.registerBlockVariation('core/group', {
+	name: 'full-width-group',
+	title: 'Full width group',
+	attributes: {
+		align: 'full',
+	},
+	isDefault: true,
+});
---
+++ includes/js/editor.js
@@ -0,0 +1,15 @@
+wp.blocks.registerBlockVariation('core/group', {
+	name: 'full-width-group',
+	title: 'Full width group',
+	attributes: {
+		align: 'full',
+	},
+});
+wp.blocks.registerBlockVariation('core/group', {
+	name: 'full-width-group',
+	title: 'Full width group',
+	attributes: {
+		align: 'full',
+	},
+	isDefault: true,
+});
