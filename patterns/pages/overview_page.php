5c44fae3f49cada943363c986be6da0dcc09b0e5
Darya Shokouhi <darya.shokouhi@ubc.ca>
2022-08-22 13:00:46 -0700

    More global bug fixes

--- patterns/pages/overview_page.php
+++ patterns/pages/overview_page.php
@@ -1,236 +1,91 @@
 <!-- wp:group {"className":"overview_page"} -->
-<div class="wp-block-group overview_page"><!-- wp:paragraph {"fontSize":"medium"} -->
-<p class="has-medium-font-size">
-        Introduction copy about the section. Ut enim ad minim veniam, quis
-        nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-        esse.
-    </p>
-<!-- /wp:paragraph -->
+<div class="wp-block-group overview_page">
+  <!-- wp:paragraph {"fontSize":"medium"} -->
+  <p class="has-medium-font-size">
+    Introduction copy about the section. Ut enim ad minim veniam, quis nostrud
+    exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis
+    aute irure dolor in reprehenderit in voluptate velit esse.
+  </p>
+  <!-- /wp:paragraph -->
 
-<!-- wp:group {"className":"overview_page_columns"} -->
-<div class="wp-block-group overview_page_columns"><!-- wp:group -->
-<div class="wp-block-group"><!-- wp:image -->
-<figure class="wp-block-image"><img alt=""/></figure>
-<!-- /wp:image -->
+  <!-- wp:group {"className":"overview_page_columns"} -->
+  <div class="wp-block-group overview_page_columns">
+    <!-- wp:group -->
+    <div class="wp-block-group">
+      <!-- wp:image -->
+      <figure class="wp-block-image"><img alt="" /></figure>
+      <!-- /wp:image -->
 
-<!-- wp:heading -->
-<h2>Section 1</h2>
-<!-- /wp:heading -->
+      <!-- wp:heading -->
+      <h2>Section 1</h2>
+      <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>The “in-house” role is part of the new Embedded Counsellor Program (ECP), which provides students with direct access to resources that are tailored to their specific needs and concerns. We met with Asmae (pronounced Ess-maa) to learn more about the unique supports available for graduate students. Heading 2 Styling for Questions or Sections My priority […]</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <p>
+        The “in-house” role is part of the new Embedded Counsellor Program
+        (ECP), which provides students with direct access to resources that are
+        tailored to their specific needs and concerns. We met with Asmae
+        (pronounced Ess-maa) to learn more about the unique supports available
+        for graduate students. Heading 2 Styling for Questions or Sections My
+        priority […]
+      </p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
 
-<!-- wp:group -->
-<div class="wp-block-group"><!-- wp:image -->
-<figure class="wp-block-image"><img alt=""/></figure>
-<!-- /wp:image -->
+    <!-- wp:group -->
+    <div class="wp-block-group">
+      <!-- wp:image -->
+      <figure class="wp-block-image"><img alt="" /></figure>
+      <!-- /wp:image -->
 
-<!-- wp:heading -->
-<h2>Section 2</h2>
-<!-- /wp:heading -->
+      <!-- wp:heading -->
+      <h2>Section 2</h2>
+      <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>The “in-house” role is part of the new Embedded Counsellor Program (ECP), which provides students with direct access to resources that are tailored to their specific needs and concerns. We met with Asmae (pronounced Ess-maa) to learn more about the unique supports available for graduate students. Heading 2 Styling for Questions or Sections My priority […]</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <p>
+        The “in-house” role is part of the new Embedded Counsellor Program
+        (ECP), which provides students with direct access to resources that are
+        tailored to their specific needs and concerns. We met with Asmae
+        (pronounced Ess-maa) to learn more about the unique supports available
+        for graduate students. Heading 2 Styling for Questions or Sections My
+        priority […]
+      </p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
 
-<!-- wp:group -->
-<div class="wp-block-group"><!-- wp:image -->
-<figure class="wp-block-image"><img alt=""/></figure>
-<!-- /wp:image -->
+    <!-- wp:group -->
+    <div class="wp-block-group">
+      <!-- wp:image -->
+      <figure class="wp-block-image"><img alt="" /></figure>
+      <!-- /wp:image -->
 
-<!-- wp:heading -->
-<h2>Section 3</h2>
-<!-- /wp:heading -->
+      <!-- wp:heading -->
+      <h2>Section 3</h2>
+      <!-- /wp:heading -->
 
-<!-- wp:paragraph -->
-<p>The “in-house” role is part of the new Embedded Counsellor Program (ECP), which provides students with direct access to resources that are tailored to their specific needs and concerns. We met with Asmae (pronounced Ess-maa) to learn more about the unique supports available for graduate students. Heading 2 Styling for Questions or Sections My priority […]</p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
+      <!-- wp:paragraph -->
+      <p>
+        The “in-house” role is part of the new Embedded Counsellor Program
+        (ECP), which provides students with direct access to resources that are
+        tailored to their specific needs and concerns. We met with Asmae
+        (pronounced Ess-maa) to learn more about the unique supports available
+        for graduate students. Heading 2 Styling for Questions or Sections My
+        priority […]
+      </p>
+      <!-- /wp:paragraph -->
+    </div>
+    <!-- /wp:group -->
+  </div>
+  <!-- /wp:group -->
 
-<!-- wp:group -->
-<div class="wp-block-group"><!-- wp:columns {"textColor":"white","className":"blue_feature_blocks"} -->
-<div class="wp-block-columns blue_feature_blocks has-white-color has-text-color"><!-- wp:column {"width":"33.33%","style":{"color":{"background":"#002145"}},"textColor":"white"} -->
-<div class="wp-block-column has-white-color has-text-color has-background" style="background-color:#002145;flex-basis:33.33%"><!-- wp:heading {"level":3,"textColor":"white"} -->
-<h3 class="has-white-color has-text-color">Calendar</h3>
-<!-- /wp:heading -->
+<?php include MBP_PATTERNS_FOLDER . 'components/blue_feature_blocks.php'; ?>
 
-<!-- wp:paragraph {"textColor":"white"} -->
-<p class="has-white-color has-text-color">
-                    Lorem ipsum dolor sit amet, adipisicing elit, sed do eiusmod
-                    tempor incididunt ut ero labore.
-                </p>
-<!-- /wp:paragraph -->
+<?php include MBP_PATTERNS_FOLDER . 'components/quick_links_three_columns.php'; ?>
 
-<!-- wp:paragraph {"textColor":"white"} -->
-<p class="has-white-color has-text-color">
-                    <a rel="noreferrer noopener" href="https://mednet.med.ubc.ca/" data-type="URL" data-id="https://mednet.med.ubc.ca/" target="_blank">Calendar of Events</a>
-                </p>
-<!-- /wp:paragraph -->
+<?php include MBP_PATTERNS_FOLDER . 'components/contact_block.php'; ?>
 
-<!-- wp:paragraph {"textColor":"white"} -->
-<p class="has-white-color has-text-color">
-                    Faculty of Medicine News
-                </p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"66.66%","style":{"color":{"background":"#0055b7"}},"textColor":"white"} -->
-<div class="wp-block-column has-white-color has-text-color has-background" style="background-color:#0055b7;flex-basis:66.66%"><!-- wp:group -->
-<div class="wp-block-group"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"24px","letterSpacing":"-0.24px"}},"textColor":"white"} -->
-<h3 class="has-white-color has-text-color" style="font-size:24px;letter-spacing:-0.24px">
-                        Stories
-                    </h3>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"textColor":"white"} -->
-<p class="has-white-color has-text-color">
-                        Learn more about What’s New in the Faculty of Medicine
-                        and at UBC.
-                    </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"textColor":"white","style":{"typography":{"fontSize":"16px"},"border":{"radius":"0px"},"color":{"background":"#002145"}},"className":"is-style-fill"} -->
-<div class="wp-block-button has-custom-font-size is-style-fill" style="font-size:16px"><a class="wp-block-button__link has-white-color has-text-color has-background" href="/" style="border-radius:0px;background-color:#002145" target="_blank" rel="noreferrer noopener">See all News &amp; Honours
-                                <i class="icon fa fa-clone" aria-hidden="true">
-                                </i></a></div>
-<!-- /wp:button --></div>
-<!-- /wp:buttons --></div>
-<!-- /wp:group --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"className":"quick_links_three_columns"} -->
-<div class="wp-block-group quick_links_three_columns"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","letterSpacing":"-0.18px"}}} -->
-<h4 style="font-size:18px;font-style:normal;font-weight:600;letter-spacing:-0.18px">
-                    Quicklinks Category
-                </h4>
-<!-- /wp:heading -->
-
-<!-- wp:list {"style":{"typography":{"fontSize":"18px"}}} -->
-<ul style="font-size:18px"><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Longer Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Office of Creative and Communications</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Longer Resource Link</a>
-                    </li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","letterSpacing":"-0.18px"}}} -->
-<h4 style="font-size:18px;font-style:normal;font-weight:600;letter-spacing:-0.18px">
-                    Quicklinks Category
-                </h4>
-<!-- /wp:heading -->
-
-<!-- wp:list {"style":{"typography":{"fontSize":"18px"}}} -->
-<ul style="font-size:18px"><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Longer Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Office of Creative and Communications</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Longer Resource Link</a>
-                    </li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column -->
-
-<!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","letterSpacing":"-0.18px"}}} -->
-<h4 style="font-size:18px;font-style:normal;font-weight:600;letter-spacing:-0.18px">
-                    Quicklinks Category
-                </h4>
-<!-- /wp:heading -->
-
-<!-- wp:list {"style":{"typography":{"fontSize":"18px"}}} -->
-<ul style="font-size:18px"><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Longer Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Office of Creative and Communications</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Resource Link</a>
-                    </li><li>
-                        <a href="/" target="_blank" rel="noreferrer noopener">Longer Resource Link</a>
-                    </li></ul>
-<!-- /wp:list --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group -->
-
-<!-- wp:group {"style":{"color":{"background":"#f2f2f2"}},"className":"contact_block"} -->
-<div class="wp-block-group contact_block has-background" style="background-color:#f2f2f2"><!-- wp:columns -->
-<div class="wp-block-columns"><!-- wp:column {"width":"25%"} -->
-<div class="wp-block-column" style="flex-basis:25%"><!-- wp:image {"align":"center","id":471,"width":200,"height":200,"sizeSlug":"full","linkDestination":"none","className":"is-style-rounded"} -->
-<figure class="wp-block-image aligncenter size-full is-resized is-style-rounded"><img src="https://via.placeholder.com/150/002145/ffffff/" alt="" class="wp-image-471" width="200" height="200"/></figure>
-<!-- /wp:image --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"40%"} -->
-<div class="wp-block-column" style="flex-basis:40%"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"18px","letterSpacing":"-0.18px"}}} -->
-<h4 style="font-size:18px;letter-spacing:-0.18px">
-                    Contact
-                </h4>
-<!-- /wp:heading -->
-
-<!-- wp:heading {"style":{"typography":{"fontSize":"30px","letterSpacing":"-0.3px"}}} -->
-<h2 style="font-size:30px;letter-spacing:-0.3px">
-                    FirstName LastName
-                </h2>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
-<p style="font-size:16px">Title, Department</p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
-<p style="font-size:16px">
-                    Email:
-                    <a href="mailto:name.name@ubc.ca">name.name@ubc.ca </a><br>Phone: 123.456.7890
-                </p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column -->
-
-<!-- wp:column {"width":"35%"} -->
-<div class="wp-block-column" style="flex-basis:35%"><!-- wp:heading {"level":4,"style":{"typography":{"fontSize":"18px","fontStyle":"normal","fontWeight":"600","letterSpacing":"-0.18px"}}} -->
-<h4 style="font-size:18px;font-style:normal;font-weight:600;letter-spacing:-0.18px">
-                    Additional Information
-                </h4>
-<!-- /wp:heading -->
-
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
-<p style="font-size:16px">
-                    Visit the
-                    <a href="/" target="_blank" rel="noreferrer noopener">Office of Lorem Ipsum Dolor Sit Amet </a>contact page for more information about who we are and what
-                    we do.
-                </p>
-<!-- /wp:paragraph -->
-
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
-<p style="font-size:16px">
-                    View the
-                    <a href="/" target="_blank" rel="noreferrer noopener">Office of Dolore Magna Aliqua Commodo Consequat.</a>
-                </p>
-<!-- /wp:paragraph --></div>
-<!-- /wp:column --></div>
-<!-- /wp:columns --></div>
-<!-- /wp:group --></div>
+</div>
 <!-- /wp:group -->
