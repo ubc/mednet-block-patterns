daf81177b23786286fc2b89cf06befc5825a6acf
Graham Douglas <graham.douglas@ubc.ca>
2022-08-22 17:31:52 -0700

    Base editor fonts, one column

--- patterns/layouts/one_column.php
+++ patterns/layouts/one_column.php
@@ -1,11 +1,13 @@
 <!-- wp:group {"className":"one_column_page"} -->
-<div class="wp-block-group one_column_page"><!-- wp:paragraph {"placeholder":"Introduction content goes here","fontSize":"medium"} -->
-<p class="has-medium-font-size"></p>
-<!-- /wp:paragraph -->
+<div class="wp-block-group one_column_page">
+    <!-- wp:paragraph {"placeholder":"Introduction content goes here","fontSize":"large"} -->
+    <p class="has-large-font-size"></p>
+    <!-- /wp:paragraph -->
 
-<!-- wp:separator {"style":{"color":{"background":"#e6e6e6"}},"className":"is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color:#e6e6e6;color:#e6e6e6"/>
-<!-- /wp:separator --></div>
+    <!-- wp:separator {"className":"is-style-wide"} -->
+    <hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" />
+    <!-- /wp:separator -->
+</div>
 <!-- /wp:group -->
 
 <!-- wp:paragraph {"placeholder":"Page content"} -->
