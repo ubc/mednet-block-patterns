ac92dbf76dcafeeae86088cd42b5035222bb8f9a
Darya Shokouhi <darya.shokouhi@ubc.ca>
2022-09-08 14:49:20 -0700

    removing inline styling

--- patterns/components/three_col_block_with_blue_feature_box.php
+++ patterns/components/three_col_block_with_blue_feature_box.php
@@ -1,32 +1,36 @@
+<!-- wp:paragraph -->
+<p></p>
+<!-- /wp:paragraph -->
+
 <!-- wp:group {"className":"three_col_block_with_feature_box"} -->
 <div class="wp-block-group three_col_block_with_feature_box"><!-- wp:columns -->
 <div class="wp-block-columns"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"30px","letterSpacing":"-0.3px"},"color":{"text":"#002145"}}} -->
-<h2 class="has-text-color" style="color:#002145;font-size:30px;letter-spacing:-0.3px">What&rsquo;s New</h2>
+<div class="wp-block-column"><!-- wp:heading -->
+<h2>What’s New</h2>
 <!-- /wp:heading -->
 
-<!-- wp:list {"style":{"color":{"text":"#0055b7"},"typography":{"fontSize":"16px"}}} -->
-<ul class="has-text-color" style="color:#0055b7;font-size:16px"><li><a href="/">Our Strategic Direction</a></li><li><a href="/">Faculty Executive Minutes</a></li><li><a href="/">Graduate Spotlight</a></li><li><a href="/">Events</a></li><li><a href="/">News &amp; Honours</a></li><li><a href="/">Dates &amp; Deadlines</a></li><li><a href="/">Our Strategic Direction</a></li><li><a href="/">Faculty Executive Minutes</a></li><li><a href="/">Graduate Spotlight</a></li><li><a href="/">Events</a></li><li><a href="/">Dates &amp; Deadlines</a></li></ul>
+<!-- wp:list -->
+<ul><li><a href="/">Our Strategic Direction</a></li><li><a href="/">Faculty Executive Minutes</a></li><li><a href="/">Graduate Spotlight</a></li><li><a href="/">Events</a></li><li><a href="/">News &amp; Honours</a></li><li><a href="/">Dates &amp; Deadlines</a></li><li><a href="/">Our Strategic Direction</a></li><li><a href="/">Faculty Executive Minutes</a></li><li><a href="/">Graduate Spotlight</a></li><li><a href="/">Events</a></li><li><a href="/">Dates &amp; Deadlines</a></li></ul>
 <!-- /wp:list --></div>
 <!-- /wp:column -->
 
 <!-- wp:column -->
-<div class="wp-block-column"><!-- wp:heading {"style":{"typography":{"fontSize":"30px"},"color":{"text":"#002145"}}} -->
-<h2 class="has-text-color" style="color:#002145;font-size:30px">Useful Links</h2>
+<div class="wp-block-column"><!-- wp:heading -->
+<h2>Useful Links</h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontSize":"16px"}}} -->
-<p style="font-size:16px">The <a rel="noreferrer noopener" href="https://mednet.med.ubc.ca/AboutUs/ContactUs/Pages/default.aspx" data-type="URL" data-id="https://mednet.med.ubc.ca/AboutUs/ContactUs/Pages/default.aspx" target="_blank">contact list</a> for key contacts for units, departments and centres. To request updates, please email: <a href="mailto:meddean.email@ubc.ca" data-type="mailto" data-id="mailto:meddean.email@ubc.ca" target="_blank" rel="noreferrer noopener">meddean.email@ubc.ca</a>.</p>
+<!-- wp:paragraph -->
+<p>The <a rel="noreferrer noopener" href="https://mednet.med.ubc.ca/AboutUs/ContactUs/Pages/default.aspx" data-type="URL" data-id="https://mednet.med.ubc.ca/AboutUs/ContactUs/Pages/default.aspx" target="_blank">contact list</a> for key contacts for units, departments and centres. To request updates, please email: <a href="mailto:meddean.email@ubc.ca" data-type="mailto" data-id="mailto:meddean.email@ubc.ca" target="_blank" rel="noreferrer noopener">meddean.email@ubc.ca</a>.</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"center","style":{"color":{"background":"#002145"}}} -->
-<div class="wp-block-column is-vertically-aligned-center has-background" style="background-color:#002145"><!-- wp:heading {"textAlign":"left","style":{"typography":{"fontSize":"30px","letterSpacing":"-0.3px","fontStyle":"normal","fontWeight":"600"}},"textColor":"white"} -->
-<h2 class="has-text-align-left has-white-color has-text-color" style="font-size:30px;font-style:normal;font-weight:600;letter-spacing:-0.3px">Workday Support</h2>
+<div class="wp-block-column is-vertically-aligned-center has-background" style="background-color:#002145"><!-- wp:heading {"textAlign":"left","textColor":"white"} -->
+<h2 class="has-text-align-left has-white-color has-text-color">Workday Support</h2>
 <!-- /wp:heading -->
 
-<!-- wp:paragraph {"align":"left","style":{"typography":{"fontSize":"16px"}},"textColor":"white"} -->
-<p class="has-text-align-left has-white-color has-text-color" style="font-size:16px">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Click here to find out more. This is a 3-column text component with the third column highlighted in a grey box. The height of all columns is shared. Specifically, the height of this box is dependent on the height of the column with the longest content (i.e. Column 1).</p>
+<!-- wp:paragraph {"align":"left","textColor":"white","className":"is-style-default"} -->
+<p class="has-text-align-left is-style-default has-white-color has-text-color">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut ero labore et dolore magna aliqua. Click here to find out more. This is a 3-column text component with the third column highlighted in a grey box. The height of all columns is shared. Specifically, the height of this box is dependent on the height of the column with the longest content (i.e. Column 1).</p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
