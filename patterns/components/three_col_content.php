9a6c04c006f50e3576ce0a937cbfe53ea0c70dbc
Graham Douglas <graham.douglas@ubc.ca>
2022-10-24 11:08:41 -0700

    three column pattern fixes

--- patterns/components/three_col_content.php
+++ patterns/components/three_col_content.php
@@ -1,6 +1,6 @@
 <!-- wp:group {"className":"three-col-block-with-feature-box"} -->
 <div class="wp-block-group three-col-block-with-feature-box">
-    <!-- wp:columns -->
+    <!-- wp:columns {"verticalAlignment":null} -->
     <div class="wp-block-columns">
         <!-- wp:column -->
         <div class="wp-block-column">
@@ -10,27 +10,44 @@
 
             <!-- wp:list -->
             <ul>
+                <!-- wp:list-item -->
                 <li></li>
+                <!-- /wp:list-item -->
             </ul>
             <!-- /wp:list -->
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column">
-            <!-- wp:heading {"placeholder":"Heading"} -->
-            <h2></h2>
-            <!-- /wp:heading -->
+        <!-- wp:column {"className":"is-style-default"} -->
+        <div class="wp-block-column is-style-default">
+            <!-- wp:group {"className":"is-style-default-group","layout":{"type":"constrained"}} -->
+            <div class="wp-block-group is-style-default-group">
+                <!-- wp:heading {"placeholder":"Heading"} -->
+                <h2></h2>
+                <!-- /wp:heading -->
 
-            <!-- wp:paragraph {"placeholder":"Minima facere quae quod inventore dolore aspernatur nostrum officia sit architecto vero magni porro iure consequatur, animi eaque fugit numquam quibusdam voluptatem."} -->
-            <p></p>
-            <!-- /wp:paragraph -->
+                <!-- wp:paragraph {"placeholder":"Minima facere quae quod inventore dolore aspernatur nostrum officia sit architecto vero magni porro iure consequatur, animi eaque fugit numquam quibusdam voluptatem."} -->
+                <p></p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
         </div>
         <!-- /wp:column -->
 
-        <!-- wp:column {"verticalAlignment":"top","className":"feature-block-column"} -->
-        <div class="wp-block-column is-vertically-aligned-top feature-block-column">
-            <?php require MBP_PATTERNS_FOLDER . 'text/sidebar_block.php'; ?>
+        <!-- wp:column {"style":{"spacing":{"blockGap":"0px"}},"className":"feature-block-column is-style-reverse-column"} -->
+        <div
+            class="wp-block-column feature-block-column is-style-reverse-column">
+            <!-- wp:group {"layout":{"type":"constrained"}} -->
+            <div class="wp-block-group">
+                <!-- wp:heading {"placeholder":"Heading"} -->
+                <h2></h2>
+                <!-- /wp:heading -->
+
+                <!-- wp:paragraph {"placeholder":"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."} -->
+                <p></p>
+                <!-- /wp:paragraph -->
+            </div>
+            <!-- /wp:group -->
         </div>
         <!-- /wp:column -->
     </div>
